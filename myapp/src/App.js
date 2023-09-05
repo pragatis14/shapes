c72b2ca1b536844eb79373ae9f5f04b9f1e23f68
pragatis14 <pragatisingh1497@gmail.com>
2023-09-05 21:24:38 +0530

    React changes for project

---
+++ myapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Shape from './components/Shape';
+
+function App() {
+  return (
+    <div className="App">
+      <Shape />
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/Shape.js
@@ -0,0 +1,32 @@
+import React,{useState} from "react";
+import './Shape.css';
+export default function Shape(){
+    const[shapeArr, setShapeArray]=useState([]);
+    const[shape, setShape]=useState("");
+    const handleChangeDropDown=(e)=>{
+        setShape(e.target.value);
+    }
+    const handleChange=()=>{
+        switch(shape){
+            case "Square": setShapeArray([...shapeArr,0]); break;
+            case "Circle": setShapeArray([...shapeArr,1]); break;
+            default: return;
+        }
+    }
+    return(
+        <div>
+            <select name="cars" id="cars" onChange={e=>handleChangeDropDown(e)}>
+                <option value="Square">Square</option>
+                <option value="Circle">Circle</option>
+            </select>
+            <button id="shapes-holder" onClick={handleChange}>Add Shape</button>
+            {
+                shapeArr.map((shape, index)=>(
+                    <div className={shape==0 ? 'square':'circle'} key={index}>
+                        {index}
+                    </div> 
+                ))
+            }
+        </div>
+    )
+}
